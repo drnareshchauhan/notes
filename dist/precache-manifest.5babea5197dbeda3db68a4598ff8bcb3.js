0d546194b14be90109f2dadce2f006bcca02363a
drnareshchauhan <drnareshchauhan@gmail.com>
2021-08-14 15:43:03 +0530

    note2

---
+++ dist/precache-manifest.5babea5197dbeda3db68a4598ff8bcb3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e3d893df87131d19ecb56024c2a9093",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddac643723f4a29b9829",
+    "url": "/static/css/main.1c3db1b0.chunk.css"
+  },
+  {
+    "revision": "e3175e3dfb4b5ecd79f4",
+    "url": "/static/js/2.c22bee0b.chunk.js"
+  },
+  {
+    "revision": "c06d497d6716e84111c2fd9253bcfc02",
+    "url": "/static/js/2.c22bee0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddac643723f4a29b9829",
+    "url": "/static/js/main.6aeaace9.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "734fa2b987f0736bc376b059edf4d20b",
+    "url": "/static/media/imp.734fa2b9.png"
+  }
+]);
